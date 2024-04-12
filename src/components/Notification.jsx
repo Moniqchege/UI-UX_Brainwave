acf66bba3866589101bfaa8b60203b40d05acb85
Moniqchege <quichege33@gmail.com>
2024-04-12 18:27:14 +0300

    initial commit

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,19 +4,21 @@ import { notificationImages } from "../constants";
 const Notification = ({ className, title }) => {
   return (
     <div
-      className={`${className || ""
-        } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
+      className={`${
+        className || ""
+      } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
     >
       <img
         src={notification1}
         width={62}
         height={62}
-        alt='image'
+        alt="image"
         className="rounded-xl"
       />
 
       <div className="flex-1">
         <h6 className="mb-1 font-semibold text-base">{title}</h6>
+
         <div className="flex items-center justify-between">
           <ul className="flex -m-0.5">
             {notificationImages.map((item, index) => (
@@ -41,4 +43,4 @@ const Notification = ({ className, title }) => {
   );
 };
 
-export default Notification
+export default Notification;
