5ab5d03335e725eac0afed3361637778f76af722
Moniqchege <quichege33@gmail.com>
2024-04-12 15:08:22 +0300

    initial commit

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,18 @@
+import Heading from "./Heading"
+import Section from "./Section"
+
+const Benefits = () => {
+  return (
+    <Section id='features'>
+      <div
+      className="container relative z-2"
+      >
+        <Heading className='' title='Chat Smarter, Not Harder With Brainwave' />
+
+      </div>
+      Benefits
+    </Section>
+  )
+}
+
+export default Benefits
